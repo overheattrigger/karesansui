40db6fd8797db0067106ce34d559eac80cb72e4c
Tetsuya <murata@cerevo.com>
2016-12-15 14:09:31 +0900

    softmax関数を用いた学習方法に変更 入力もそれに合わせた

--- judge/judge.py
+++ judge/judge.py
@@ -11,10 +11,11 @@ import numpy as np
 import math
 import tensorflow as tf
 import random
+import time
 tf.logging.set_verbosity(tf.logging.INFO)
 NUM_CLASSES = 18
-IMAGE_ROWS = 600
-IMAGE_COLS = 800
+IMAGE_ROWS = 150
+IMAGE_COLS = 200
 IMAGE_PIXELS = IMAGE_ROWS*IMAGE_COLS*3
 
 flags = tf.app.flags
@@ -23,9 +24,11 @@ flags.DEFINE_string('train', 'train.txt', 'File name of train data')
 flags.DEFINE_string('test', 'test.txt', 'File name of train data')
 flags.DEFINE_string('train_dir', '/tmp/data', 'Directory to put the training data.')
 flags.DEFINE_integer('max_steps', 1000, 'Number of steps to run trainer.')
-flags.DEFINE_integer('batch_size', 50, 'Batch size'
+flags.DEFINE_integer('batch_size', 30, 'Batch size'
                      'Must divide evenly into the dataset sizes.')
 flags.DEFINE_float('learning_rate', 1e-4, 'Initial learning rate.')
+flags.DEFINE_integer('hidden1', 128, 'Number of steps to run trainer.')
+flags.DEFINE_integer('hidden2', 32, 'Number of steps to run trainer.')
 
 def inference(images, hidden1_units, hidden2_units):
   """Build the MNIST model up to where it may be used for inference.
@@ -132,6 +135,24 @@ def evaluation(logits, labels):
   # Return the number of true entries.
   return tf.reduce_sum(tf.cast(correct, tf.int32))
 
+def do_eval(sess, eval_correct, test_data, test_label):
+  """Runs one evaluation against the full epoch of data.
+
+  Args:
+    sess: The session in which the model has been trained.
+    eval_correct: The Tensor that returns the number of correct predictions.
+    images_placeholder: The images placeholder.
+    labels_placeholder: The labels placeholder.
+    data_set: The set of images and labels to evaluate, from
+      input_data.read_data_sets().
+  """
+  # And run one epoch of eval.
+  true_count = 0  # Counts the number of correct predictions.
+
+  true_count += sess.run(eval_correct, feed_dict={x: test_data, y_:test_label})
+  precision = float(true_count) / len(test_label)
+  print('  Num examples: %d  Num correct: %d  Precision @ 1: %0.04f' %
+        (len(test_label), true_count, precision))
 
 if __name__ == '__main__':
 
@@ -152,6 +173,7 @@ if __name__ == '__main__':
     f = open('test.txt', 'r')
     test_image = []
     test_label = []
+    test_list = []
     for line in f:
         line = line.rstrip()
         l = line.split()
@@ -159,20 +181,20 @@ if __name__ == '__main__':
         # img = cv2.resize(img, (28, 28))
         test_image.append(img.flatten().astype(np.float32)/255.0)
         tmp = np.zeros(NUM_CLASSES)
-        tmp[int(l[1])] = 1
-        test_label.append(tmp)
+        # tmp[int(l[1])] = 1
+        test_list.append(l)
+        test_label.append(l[1])
     test_image = np.asarray(test_image)
     test_label = np.asarray(test_label)
     f.close()
     
     # Create the model
-    x = tf.placeholder(tf.float32, [None, IMAGE_PIXELS])
-    W = tf.Variable(tf.zeros([IMAGE_PIXELS, NUM_CLASSES]))
-    b = tf.Variable(tf.zeros([NUM_CLASSES]))
-    y = tf.matmul(x, W) + b
-
-    # Define loss and optimizer
-    y_ = tf.placeholder(tf.float32, [None, NUM_CLASSES])
+    x = tf.placeholder(tf.float32, shape=(FLAGS.batch_size, IMAGE_PIXELS))
+    y_ = tf.placeholder(tf.int32, shape=(FLAGS.batch_size))
+    logits = inference(x, FLAGS.hidden1, FLAGS.hidden2)
+    loss_ = loss(logits, y_)
+    train_op = training(loss_, FLAGS.learning_rate)
+    eval_correct = evaluation(logits, y_)
 
     # The raw formulation of cross-entropy,
     #
@@ -183,8 +205,8 @@ if __name__ == '__main__':
     #
     # So here we use tf.nn.softmax_cross_entropy_with_logits on the raw
     # outputs of 'y', and then average across the batch.
-    cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y, y_))
-    train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+    # cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y, y_))
+    # train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 
     # 保存の準備
     saver = tf.train.Saver()
@@ -197,8 +219,9 @@ if __name__ == '__main__':
     # summary_writer = tf.train.SummaryWriter(FLAGS.train_dir, sess.graph_def)
 
     # Train
-    for j in range(FLAGS.max_steps):
-        print ("start train! %d" % j )
+    for step in range(FLAGS.max_steps):
+        start_time = time.time()
+        print ("start train! %d" % step )
         l = random.sample(train_list, FLAGS.batch_size)
         train_image = []
         train_label = []
@@ -210,22 +233,46 @@ if __name__ == '__main__':
             train_image.append(img.flatten().astype(np.float32)/255.0)
             # ラベルを1-of-k方式で用意する
             tmp = np.zeros(NUM_CLASSES)
-            tmp[int(i[1])] = 1
-            train_label.append(tmp)
+            # tmp[int(i[1])] = 1
+            train_label.append(i[1])
             #numpy形式に変換
         train_image = np.asarray(train_image)
         train_label = np.asarray(train_label)
-        sess.run(train_step, feed_dict={x: train_image, y_: train_label})
-        if ((j % 100) == 0):
-            correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
-            accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-            print ("step %d, training accuracy %g"%(j, sess.run(accuracy, feed_dict={x: test_image, y_: test_label})))
+        _, loss_value = sess.run([train_op, loss_], feed_dict={x: train_image, y_: train_label})
+        duration = time.time() - start_time
+        if step % 100 == 0:
+            # Print status to stdout.
+            print('Step %d: loss = %.2f (%.3f sec)' % (step, loss_value, duration))
+        # Save a checkpoint and evaluate the model periodically.
+        if (step + 1) % 100 == 0 or (step + 1) == FLAGS.max_steps:
+            l = random.sample(test_list, FLAGS.batch_size)
+            test_image = []
+            test_label = []
+            for i in l:
+                img = cv2.imread(i[0])
+
+                # 一列にした後、0-1のfloat値にする
+                test_image.append(img.flatten().astype(np.float32)/255.0)
+                # ラベルを1-of-k方式で用意する
+                tmp = np.zeros(NUM_CLASSES)
+                # tmp[int(i[1])] = 1
+                test_label.append(i[1])
+                #numpy形式に変換
+            test_image = np.asarray(test_image)
+            test_label = np.asarray(test_label)
+            # Evaluate against the test set.
+            print('Test Data Eval:')
+            do_eval(sess, eval_correct, test_image, test_label)
+            # Update the events file.
+            # summary_str = sess.run(summary, feed_dict=feed_dict)
+            # summary_writer.add_summary(summary_str, step)
+            # summary_writer.flush()
 
     # Test trained model
-    correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
-    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-    print(sess.run(accuracy, feed_dict={x: test_image,
-                                        y_: test_label}))
+    # correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+    # accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    # print(sess.run(accuracy, feed_dict={x: test_image,
+    #                                     y_: test_label}))
 
     # 最終的なモデルを保存
     save_path = saver.save(sess, "model.ckpt", global_step=FLAGS.max_steps)
